5d86298fd346f2e5a83ee5b127d34a26dfb6b9c6
Bala Vinaithirthan <57514761+balavinaithirthan@users.noreply.github.com>
2025-02-02 15:06:44 -0800

    part 0 code, almost done

--- libunix/find-ttyusb.c
+++ libunix/find-ttyusb.c
@@ -1,26 +1,35 @@
 // engler, cs140e: your code to find the tty-usb device on your laptop.
+#include "dirent.h"
+#include "libunix.h"
 #include <assert.h>
 #include <fcntl.h>
 #include <string.h>
-
-#include "libunix.h"
+#include <sys/stat.h>
 
 #define _SVID_SOURCE
 #include <dirent.h>
 static const char *ttyusb_prefixes[] = {
-    "ttyUSB",	// linux
-    "ttyACM",   // linux
-    "cu.SLAB_USB", // mac os
+    "ttyUSB",       // linux
+    "ttyACM",       // linux
+    "cu.SLAB_USB",  // mac os
     "cu.usbserial", // mac os
-    // if your system uses another name, add it.
-	0
-};
+                    // if your system uses another name, add it.
+    0};
 
 static int filter(const struct dirent *d) {
-    // scan through the prefixes, returning 1 when you find a match.
-    // 0 if there is no match.
-    unimplemented();
+  // scan through the prefixes, returning 1 when you find a match.
+  // 0 if there is no match.
+  for (int i = 0; i < 4; i++) {
+    if (strncmp(d->d_name, ttyusb_prefixes[i], strlen(ttyusb_prefixes[i])) ==
+        0) {
+      return 1;
+    }
+  }
+  return 0;
 }
+
+static int (*FUNCTION)(const struct dirent **,
+                       const struct dirent **) = alphasort;
 
 // find the TTY-usb device (if any) by using <scandir> to search for
 // a device with a prefix given by <ttyusb_prefixes> in /dev
@@ -28,21 +37,53 @@ static int filter(const struct dirent *d) {
 //  - device name.
 // error: panic's if 0 or more than 1 devices.
 char *find_ttyusb(void) {
-    // use <alphasort> in <scandir>
-    // return a malloc'd name so doesn't corrupt.
-    unimplemented();
+  printf("finding ttyusb\n");
+  struct dirent **namelist;
+  int n = -1;
+  n = scandir("/dev", &namelist, filter, FUNCTION);
+  if (n > 1) {
+    panic("There are too many usb options");
+  } else if (n < 0) {
+    panic("issue scanning directory with error %s\n", strerror(errno));
+  }
+  char *name = malloc(100);
+  strcat(name, "/dev/");
+  strcat(name, (*namelist)->d_name);
+  return name;
+}
+
+static int recentCompare(const struct dirent **d1, const struct dirent **d2) {
+  struct stat buf1[10000];
+  __uint32_t timeOne = 0;
+  __uint32_t timeTwo = 0;
+  char *name1 = malloc(100);
+  strcat(name1, "/dev/");
+  strcat(name1, (*d1)->d_name);
+  if (stat(name1, buf1) != 0) {
+    panic("Error reading file");
+  }
+  long time1 = buf1->st_mtime;
+  struct stat buf2[10000];
+  char *name2 = malloc(100);
+  strcat(name2, "/dev/");
+  strcat(name2, (*d1)->d_name);
+  if (stat(name2, buf2) != 0) {
+    panic("Error reading file");
+  }
+  long time2 = buf1->st_mtime;
+  return time1 < time2;
 }
 
 // return the most recently mounted ttyusb (the one
-// mounted last).  use the modification time 
-// returned by state.
+// mounted last).  use the modification time
+// returned by stat.
 char *find_ttyusb_last(void) {
-    unimplemented();
+  printf("finding ttyusb LAST\n");
+  FUNCTION = recentCompare;
+  return find_ttyusb();
 }
 
 // return the oldest mounted ttyusb (the one mounted
 // "first") --- use the modification returned by
 // stat()
-char *find_ttyusb_first(void) {
-    unimplemented();
-}
+char *find_ttyusb_first(void) { unimplemented(); }
--- libunix/read-file.c
+++ libunix/read-file.c
@@ -1,31 +1,73 @@
+#include "string.h"
 #include <assert.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 #include "libunix.h"
 
-// allocate buffer, read entire file into it, return it.   
+// allocate buffer, read entire file into it, return it.
 // buffer is zero padded to a multiple of 4.
 //
 //  - <size> = exact nbytes of file.
 //  - for allocation: round up allocated size to 4-byte multiple, pad
-//    buffer with 0s. 
+//    buffer with 0s.
 //
 // fatal error: open/read of <name> fails.
 //   - make sure to check all system calls for errors.
 //   - make sure to close the file descriptor (this will
 //     matter for later labs).
-// 
+//
 void *read_file(unsigned *size, const char *name) {
-    // How: 
-    //    - use stat() to get the size of the file.
-    //    - round up to a multiple of 4.
-    //    - allocate a buffer
-    //    - zero pads to a multiple of 4.
-    //    - read entire file into buffer (read_exact())
-    //    - fclose() the file descriptor
-    //    - make sure any padding bytes have zeros.
-    //    - return it.   
-    unimplemented();
+  // How:
+  //    - use stat() to get the size of the file.
+  //    - round up to a multiple of 4.
+  //    - allocate a buffer
+  //    - zero pads to a multiple of 4.
+  //    - read entire file into buffer (read_exact())
+  //    - fclose() the file descriptor
+  //    - make sure any padding bytes have zeros.
+  //    - return it.
+  printf("The file is named %s and the size is %d \n", name, *size);
+  struct stat buf[10000];
+  char *result = NULL;
+  if (stat(name, buf) != 0) {
+    panic("Error reading file with errno %s \n", strerror(errno));
+    return result;
+  }
+  off_t bufSize = buf->st_size;
+  *size = bufSize;
+  unsigned toAdd = bufSize % 4;
+  bufSize = (4 - toAdd) + bufSize;
+  result = malloc(bufSize);
+  if (result == NULL) {
+    panic("error mallocing data");
+  };
+  FILE *file = fopen(name, "r");
+  char temp[2];
+  if (read(file->_file, result, bufSize) < *size) {
+    panic("issue with reading!");
+  }
+  // char* res;
+  // int i = 0;
+  // while ((res = fgets(temp, sizeof(temp), file))) {
+  //     int j = 0;
+  //     while (1) {
+  //         result[i] = temp[j];
+  //         if (temp[j] == '\n' || temp[j] == EOF || j == sizeof(temp) - 1) {
+  //             break;
+  //         }
+  //         //printf("\n temp[i] is %hhd and j is %d \n", result[i], j);
+  //         i += 1;
+  //         j += 1;
+  //     }
+  // }
+  // if (!feof(file)) {
+  //     panic("issue with reading file \n");
+  // }
+  // result[i] = '\0';
+  // printf("result is %s \n", result);
+  fclose(file);
+  return result;
 }
